4ba508fe0174a2c44b291210d985d6d826af315a
FatamaOkasha <fatmaokasha01@gmail.com>
2024-08-29 19:55:10 +0300

    Fix Url

--- api/server.js
+++ api/server.js
@@ -7,5 +7,5 @@ server.use(middlewares);
 server.use(router);
 
 server.listen(8000, () => {
-  console.log('JSON Server is running on http://localhost:8000');
+  console.log('JSON Server is running on http://localhost:8000/movies');
 });
