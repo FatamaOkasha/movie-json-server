39e622a37bf81eb448f0f14a3373dfde5b36596c
FatamaOkasha <fatmaokasha01@gmail.com>
2024-08-29 20:04:11 +0300

    fix some bugs

--- api/server.js
+++ api/server.js
@@ -3,9 +3,11 @@ const server = jsonServer.create();
 const router = jsonServer.router('db.json'); // Ensure this path is correct
 const middlewares = jsonServer.defaults();
 
+// Optionally set up a base path
+server.use('/api', router);
+
 server.use(middlewares);
-server.use(router);
 
 server.listen(8000, () => {
-  console.log('JSON Server is running on http://localhost:8000/movies');
+  console.log('JSON Server is running on http://localhost:8000');
 });
